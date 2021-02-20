4195ebe85ff52c4ede86da9af05c63ba17037780
Ruby Wolf <hi@ruby.digital>
2021-02-20 00:57:09 +0000

    Updated module to call models directly and use new  style with media model.

--- controllers/sxreporting.php
+++ controllers/sxreporting.php
@@ -6,8 +6,8 @@ class SxReporting extends OBFController
   {
     $this->user->require_permission('sx_reporting_module');
     
-    $reporting_model = $this->load->model('SxReporting');
-    
+    $models = OBFModels::get_instance();
+
     $data = [
       'player_id' => $this->data('player'),
       'start' => trim($this->data('start')),
@@ -20,14 +20,14 @@ class SxReporting extends OBFController
       'tuning_hours' => trim($this->data('tuning_hours')),
       'additional_fields' => $this->data('additional_fields')
     ];
-    
-    $validation = $reporting_model('validate',$data);
+
+    $validation = $models->SxReporting('validate', $data);
     if($validation[0]==false) return $validation;
-    
-    $report_csv = $reporting_model('generate',$data);
-    
+
+    $report_csv = $models->SxReporting('generate', $data);
+
     if($report_csv[1]==0) return [false,'No log data found for this reporting period.'];
-    
+
     return array(true,'Report generated ('.$report_csv[1].' items). Click the download button below.',$report_csv[0]);
   }
 }
